2c14de5135d1fc7a13ceb73ae527d222b6b41849
Phuc Nguyen <nguyendaiphuc151006@gmail.com>
2023-03-22 23:35:13 +0700

    init project

---
+++ CSDL C#/CSDL C# _ Bai tap thuc hanh/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CSDL_C____Bai_tap_thuc_hanh.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
